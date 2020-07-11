622ba988f4a8cdb77d0140a0c7ab0026ca4e4315
abilioconstantino <abilio.constantino@gmail.com>
2020-07-11 18:41:55 -0300

    Seção 4

--- ProAgil-APP/src/app/eventos/eventos.component.ts
+++ ProAgil-APP/src/app/eventos/eventos.component.ts
@@ -4,27 +4,65 @@ import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.css']
+  styleUrls: ['./eventos.component.css'],
 })
 export class EventosComponent implements OnInit {
+  _filtroLista: string;
+  get filtroLista(): string {
+    return this._filtroLista;
+  }
 
-  evento : any;
- 
+  set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista
+      ? this.filtrarEventos(this.filtroLista)
+      : this.evento;
+  }
 
-  constructor(private http: HttpClient) { }
+  eventosFiltrados: any = [];
+  evento: any = [];
+  imagemAltura = 50;
+  imagemMargem = 2;
+  mostrarImagem = false;
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.getEventos();
+ 
   }
 
-  getEventos(){
-    this.http.get('http://localhost:5000/api/values').subscribe( response => {
-      this.evento = response;
-    },
-     error => {
-       console.log(error);
-     }
+ 
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    if (!filtrarPor) {
+      return this.evento;
+    } else {
+      return this.evento.filter(
+        (evento) =>
+          evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+          evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+          evento.lote.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+          evento.dataEvento.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+      );
+    }
+  }
 
-    )
+
+  alternarImagem() {
+    this.mostrarImagem = !this.mostrarImagem;
+  }
+  getEventos() {
+    this.http.get('http://localhost:5000/api/values').subscribe(
+      (response) => {
+        
+        this.evento = response;
+        this.eventosFiltrados = response;
+      
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 }
